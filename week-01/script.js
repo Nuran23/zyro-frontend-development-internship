519b2a7dd681ee3b8b4f79784e366e1676a276a9
Muhammad Nuran Khan <khannurankhan@gmail.com>
2026-08-26 11:14:10 +0500

    Week 1 setup complete

---
+++ week-01/script.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('msg').innerText = 'Environment Setup Successful!';
+    console.log('Week 1 Test Button Clicked Successfully');
+});
